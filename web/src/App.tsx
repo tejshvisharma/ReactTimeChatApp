f7c5e57ff257bc4ac3ce46101c96db94ba53b5c7
mahatejshvi vareny swami <tejshvisharma27@gmail.com>
2026-05-25 23:43:27 +0530

    feat: integrate Clerk for authentication
    - Added @clerk/react dependency to package.json
    - Updated App component to include SignInButton, SignUpButton, and UserButton for user authentication
    - Wrapped App component with ClerkProvider in main.tsx to provide Clerk context
    - Added error handling for missing VITE_CLERK_PUBLISHABLE_KEY

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,18 @@
 
+import {  Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
+
 function App() {
-
-
   return (
     <>
-    <h1>hello world</h1>
+      <header>
+        <Show when="signed-out">
+          <SignInButton mode="modal" />
+          <SignUpButton mode="modal"/>
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+      </header>
     </>
   )
 }
